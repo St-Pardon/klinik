58713e1c028f24d7be0cffffbb3684fe2a9cff82
st-pardon <okc4pardon@gmail.com>
2022-12-18 08:35:31 +0100

    :new: added widgets

---
+++ client/src/components/widgets/widgets.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 400px;
+  box-shadow: 4px 4px 8px 0 rgba(0, 0, 0, 0.25),
+    -2px -2px 6px 0 rgba(255, 255, 255, 0.3);
+  text-align: center;
+  border-radius: 10px;
+  padding: 20px 7px;
+`;
