26936204c0865ff0c17583b228e52959de89f845
st-pardon <okc4pardon@gmail.com>
2022-12-21 13:21:37 +0100

    :hammer: updated to the containers

--- client/src/containers/container.styled.jsx
+++ client/src/containers/container.styled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const Section = styled.section`
   display: flex;
@@ -7,9 +7,37 @@ export const Section = styled.section`
   background-color: #fff;
   width: 100%;
   gap: 20px;
+  overflow-y: scroll;
+  padding: 10px;
 `;
 
 export const Row = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: flex-end;
+
+  ${(props) =>
+    props.right &&
+    css`
+      justify-content: flex-end;
+    `}
+`;
+
+export const Entry = styled.div`
+  text-align: left;
+  margin: 20px 0;
+  //   color: #84878c;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+`;
+export const Thead = styled.thead`
+  background: #0765fe;
+  color: #fff;
+`;
+
+export const Th = styled.th`
+  padding: 8px;
 `;
--- client/src/containers/overview.container.jsx
+++ client/src/containers/overview.container.jsx
@@ -1,44 +1,31 @@
 import React from "react";
+import { SubHeading } from "../components/heading/heading.component";
 import SummaryWidget from "../components/widgets/summary.widget";
-import { Section, Row } from "./container.styled";
+import { Section, Row, Entry } from "./container.styled";
 
 const OverviewContainer = ({ user_type }) => {
   return (
     <Section>
       <Row>
-        {user_type !== "patient" ? (
-          <SummaryWidget>
-            <h3>Appointment Today</h3>
-            <p>
-              <span>127</span> Appointments Today
-            </p>
-          </SummaryWidget>
-        ) : null}
-        {user_type !== "patient" ? (
-          <SummaryWidget>
-            <h3>Doctors on Duty</h3>
-            <p>
-              <span>7</span> Doctors on Duty
-            </p>
-          </SummaryWidget>
-        ) : null}
+        {/*----- widgets for patients ----- */}
+
         {user_type !== "staff" ? (
           <SummaryWidget>
-            <h3>Last Diagonisis</h3>
+            <SubHeading widget>Last Diagonisis</SubHeading>
             <p>Malaria</p>
           </SummaryWidget>
         ) : null}
         {user_type !== "staff" ? (
           <SummaryWidget>
-            <h3>Next Appointment</h3>
+            <SubHeading>Next Appointment</SubHeading>
             <p>23rd June</p>
           </SummaryWidget>
         ) : null}
         {user_type !== "staff" ? (
           <SummaryWidget>
-            <h3>Last Vitals Checked</h3>
-            <ul>
-              <li>hieght - 5.2"</li>
+            <SubHeading>Last Vitals Checked</SubHeading>
+            <ul style={{ listStyle: "none" }}>
+              <li>height - 5.2"</li>
               <li>weight - 79kg</li>
               <li>Temperature - 45 &deg; C</li>
               <li>BP - 124/232 bpm</li>
@@ -47,26 +34,133 @@ const OverviewContainer = ({ user_type }) => {
         ) : null}
         {user_type !== "staff" ? (
           <SummaryWidget>
-            <h3>Doctor's Information</h3>
+            <SubHeading>Doctor's Information</SubHeading>
             <p>Name: Dr. Stephen Gerald (M.Med)</p>
             <p>Email: Stephengerald@fmc.com</p>
+          </SummaryWidget>
+        ) : null}
+
+        {/* ----- widgets for Nurses ----- */}
+        {user_type !== "patient" ? (
+          <SummaryWidget>
+            <SubHeading>Appointment Today</SubHeading>
+            <p>
+              <span>127</span> Appointments
+            </p>
+          </SummaryWidget>
+        ) : null}
+        {user_type !== "patient" ? (
+          <SummaryWidget>
+            <SubHeading>Doctors on Duty</SubHeading>
+            <p>
+              <span>7</span> Doctors
+            </p>
+          </SummaryWidget>
+        ) : null}
+        {user_type !== "patient" ? (
+          <SummaryWidget>
+            <SubHeading>Patients Admitted</SubHeading>
+            <p>
+              <span>14</span> Patients
+            </p>
+          </SummaryWidget>
+        ) : null}
+        {user_type !== "patient" ? (
+          <SummaryWidget>
+            <SubHeading>Processed Patients</SubHeading>
+            <ul style={{ listStyle: "none" }}>
+              <li>John Snow</li>
+              <li>Tormound Gaintsbane</li>
+              <li>Bruce Wayne</li>
+              <li>Chole Sullivan</li>
+            </ul>
           </SummaryWidget>
         ) : null}
       </Row>
       <Row>
         {user_type !== "staff" ? (
           <SummaryWidget>
-            <h3>Prescription History</h3>
+            <SubHeading>Prescription History</SubHeading>
+            <Entry>
+              <p>
+                <strong>Antifeed</strong>
+              </p>
+              <p>
+                <em>13, January 2022</em>
+              </p>
+              <p>By: Dr fred michael</p>
+              <p>Dosage: 3tabs 3 times daily</p>
+              <p></p>
+            </Entry>
+            <Entry>
+              <p>
+                <strong>Pandole</strong>
+              </p>
+              <p>
+                <em>26, March 2021</em>
+              </p>
+              <p>By: Dr john Freeman</p>
+              <p>Dosage: 1 teaspoon 3 times daily</p>
+              <p></p>
+            </Entry>
           </SummaryWidget>
         ) : null}
         {user_type !== "staff" ? (
           <SummaryWidget>
-            <h3>Doctor's Entry</h3>
+            <SubHeading>Doctor's Entry</SubHeading>
+            <Entry>
+              <p>
+                <strong>Entry by Dr tony Stark</strong>
+              </p>
+              <p>
+                <em>Date: 5th June 2022</em>
+              </p>
+              <p>Symthoms: headache, sores, pain</p>
+              <p>
+                Comment: Lorem ipsum dolor, sit amet consectetur adipisicing
+                elit. Delectus veniam laborum, quas ullam, fugit, consectetur
+                illo quae autem ea asperiores saepe? Quas debitis illo natus
+                recusandae possimus atque maxime consequatur.
+              </p>
+              <p>Diagnosis: Ebola</p>
+            </Entry>
+            <Entry>
+              <p>
+                <strong>Entry by Dr fred michael</strong>
+              </p>
+              <p>
+                <em>Date: 13th January 2022</em>
+              </p>
+              <p>Symthoms: headache, sores, pain</p>
+              <p>
+                Comment: Lorem ipsum dolor, sit amet consectetur adipisicing
+                elit. Delectus veniam laborum, quas ullam, fugit, consectetur
+                illo quae autem ea asperiores saepe? Quas debitis illo natus
+                recusandae possimus atque maxime consequatur.
+              </p>
+              <p>Diagnosis: Ebola</p>
+            </Entry>
+            <Entry>
+              <p>
+                <strong>Entry by Dr fred michael</strong>
+              </p>
+              <p>
+                <em>Date: 13th January 2022</em>
+              </p>
+              <p>Symthoms: headache, sores, pain</p>
+              <p>
+                Comment: Lorem ipsum dolor, sit amet consectetur adipisicing
+                elit. Delectus veniam laborum, quas ullam, fugit, consectetur
+                illo quae autem ea asperiores saepe? Quas debitis illo natus
+                recusandae possimus atque maxime consequatur.
+              </p>
+              <p>Diagnosis: Ebola</p>
+            </Entry>
           </SummaryWidget>
         ) : null}
         {user_type !== "staff" ? (
           <SummaryWidget>
-            <h3>Lab Works</h3>
+            <SubHeading>Lab Works</SubHeading>
           </SummaryWidget>
         ) : null}
       </Row>
