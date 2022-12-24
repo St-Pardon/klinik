184d0ade0c1b8af08cb8a6a0fd5e17580f8cc112
st-pardon <okc4pardon@gmail.com>
2022-12-24 12:09:25 +0100

    :hammer: updated registration form

--- client/src/routes/registration/newPatient.registration.jsx
+++ client/src/routes/registration/newPatient.registration.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { MdKeyboardBackspace } from "react-icons/md";
 import { Link } from "react-router-dom";
 // import { Button } from "../../components/button/button.styled";
-import { Fieldset, Form, Input } from "../../components/form/form.styled";
+import { Fieldset, Form, Input, Select } from "../../components/form/form.styled";
 import { Section } from "../../containers/container.styled";
 
 const NewPatientContainer = () => {
@@ -43,6 +43,18 @@ const NewPatientContainer = () => {
           />
         </Fieldset>
         <Fieldset>
+          <label htmlFor="sex">Sex:</label>
+          <Select name="sex" id="sex">
+            <option value="" selected disabled>
+              - - - Select Patient's Sex - - - 
+            </option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="Non Binary">Non Binary</option>
+            <option value="others">Others</option>
+          </Select>
+        </Fieldset>
+        <Fieldset>
           <label htmlFor="phone">Phone No:</label>
           <Input
             type="numeric"
@@ -59,6 +71,40 @@ const NewPatientContainer = () => {
             name="address"
             placeholder="eg: 123 pyper avenue"
           />
+        </Fieldset>
+        <Fieldset>
+          <label htmlFor="blood_group">Blood Group:</label>
+          <Select name="blood_group" id="blood_group">
+            <option value="" selected disabled>
+              - - - Select Patient's Blood Group - - - 
+            </option>
+            <option value="O Positive">O+</option>
+            <option value="O Negative">O-</option>
+            <option value="A Positive">A+</option>
+            <option value="A Negative">A-</option>
+            <option value="AB Positive">AB+</option>
+          </Select>
+        </Fieldset>
+        <Fieldset>
+          <label htmlFor="genotype">Genotype:</label>
+          <Select name="genotype" id="genotype">
+            <option value="" selected disabled>
+              - - - Select Patient's Genotype - - - 
+            </option>
+            <option value="AA">AA</option>
+            <option value="AS">AS</option>
+            <option value="AC">AC</option>
+            <option value="SS">SS</option>
+            <option value="SC">SC+</option>
+          </Select>
+        </Fieldset>
+        <Fieldset>
+          <label htmlFor="disability">Disability:</label>
+          <Input type="text" id="disability" name="disability" />
+        </Fieldset>
+        <Fieldset>
+          <label htmlFor="chronic_disease">Chronic Disease:</label>
+          <Input type="text" id="chronic_disease" name="chronic_disease" />
         </Fieldset>
         <Fieldset>
           <label htmlFor="nok">Next of Kin:</label>
@@ -79,17 +125,8 @@ const NewPatientContainer = () => {
           <Input type="text" id="nok_relationship" name="nok_relationship" />
         </Fieldset>
         <Fieldset>
-          {/* 
-            {" "}
-            <Button type="btn">
-              <MdKeyboardBackspace
-                style={{ fontSize: "1.8rem", color: "black" }}
-              />{" "}
-              Back
-            </Button>
-           */}
+          <Input submit type="submit" value="Submit" />
         </Fieldset>
-        <Input submit type="submit" value="Submit" />
       </Form>
     </Section>
   );
